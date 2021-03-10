5e92e721e9e18a351addb6f46643ffe1200d228a
Ryan Yuzuki <ryuzu001@ucr.edu>
2021-03-10 01:04:13 -0800

    use gspread instead

--- update.py
+++ update.py
@@ -1,54 +1,25 @@
-from __future__ import print_function
-import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from pprint import pprint
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 
-# The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
-SAMPLE_RANGE_NAME = 'Class Data!A2:E'
+creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
 
-def main():
-    """Shows basic usage of the Sheets API.
-    Prints values from a sample spreadsheet.
-    """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
+client = gspread.authorize(creds)
 
-    service = build('sheets', 'v4', credentials=creds)
+sheet = client.open("Testing1").sheet1  # Open the spreadhseet
 
-    # Call the Sheets API
-    sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                range=SAMPLE_RANGE_NAME).execute()
-    values = result.get('values', [])
+data = sheet.get_all_records()  # Get a list of all records
 
-    if not values:
-        print('No data found.')
-    else:
-        print('Name, Major:')
-        for row in values:
-            # Print columns A and E, which correspond to indices 0 and 4.
-            print('%s, %s' % (row[0], row[4]))
+row = sheet.row_values(3)  # Get a specific row
+col = sheet.col_values(3)  # Get a specific column
+cell = sheet.cell(1,2).value  # Get the value of a specific cell
 
-if __name__ == '__main__':
-    main()
+insertRow = ["hello", 5, "red", "blue"]
+# sheet.insert_row(insertRow, 4)  # Insert the list as a row at index 4
+sheet.delete_rows(4)
+
+sheet.update_cell(2,2, "CHANGED")  # Update one cell
+
+numRows = sheet.row_count  # Get the number of rows in the sheet
